6efffbf2dbb251b80addde6a8be7c8f7c0d29b63
Kimberly Aguilar Hermoso <kimaguilar2017@gmail.com>
2022-01-18 13:57:49 -0800

    Update htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,13 @@
+// routes
 const path = require('path');
 const router = require('express').Router();
 
-// "/notes" responds with notes.html
+// "/notes" responds with the notes.html
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-// responds with index.html file
+// routes respond with the index.html
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
